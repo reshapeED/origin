c5ea0b876822f77d02e08c99d2ad85579ded8a4d
sairah <sairah.jo@gmail.com>
2014-08-05 04:50:53 -0400

    category.rb and .DS_Store edits (?)

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,6 @@ class Category < ActiveRecord::Base
 	belongs_to :user
 	has_many :article_categories
 	has_many :articles, through: :article_categories
-	
-	validates_uniqueness_of :name
+	validates_uniqueness_of :title
 
 end
